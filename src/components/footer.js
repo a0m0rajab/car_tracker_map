0ac28967b4630cf60346ed6d54b0271b45713b5f
Abdurrahman Rajab <rajab-2@windowslive.com>
2022-03-21 16:36:09 +0300

    Feat(pages): Adding main pages and information.

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import styles from '../../styles/Home.module.css';
+
+// react footer class
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <a
+                href="https://a0m0rajab.github.io"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Created by a0m0rajab
+            </a>
+        </footer>)
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,82 +1,34 @@
-import Head from 'next/head';
-
-import Map from '../components/Map';
 
 import styles from '../../styles/Home.module.css';
 
-const DEFAULT_CENTER = [38.907132, -77.036546]
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Next.js Leaflet Starter
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <Map className={styles.homeMap} center={DEFAULT_CENTER} zoom={12}>
-          {({ TileLayer, Marker, Popup }) => (
-            <>
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-              />
-              <Marker position={DEFAULT_CENTER}>
-                <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-              </Marker>
-            </>
-          )}
-        </Map>
-
-        <p className={styles.description}>
-          <code className={styles.code}>yarn create next-app -e https://github.com/colbyfayock/next-leaflet-starter</code>
-        </p>
-
+    <Layout>
         <div className={styles.grid}>
-          <a href="https://github.com/colbyfayock/next-leaflet-starter" className={styles.card}>
-            <h3>GitHub &rarr;</h3>
-            <p>See the code in action.</p>
+          <a href="./login" className={styles.card}>
+            <h3>Login &rarr;</h3>
+            <p>Login/Register page.</p>
           </a>
 
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Next.js &rarr;</h3>
-            <p>Getting started with Next.js</p>
+          <a href="./cars" className={styles.card}>
+            <h3>Cars &rarr;</h3>
+            <p>Cars List page</p>
           </a>
 
-          <a href="https://leafletjs.com/" className={styles.card}>
-            <h3>Leaflet &rarr;</h3>
-            <p>Mapping features and APIs.</p>
+          <a href="./cars/map" className={styles.card}>
+            <h3>Cars Map &rarr;</h3>
+            <p>Cars Map page.</p>
           </a>
 
-          <a href="https://react-leaflet.js.org/" className={styles.card}>
-            <h3>React Leaflet &rarr;</h3>
-            <p>Native Leaflet components in React</p>
+          <a href="./profile" className={styles.card}>
+            <h3>profile page &rarr;</h3>
+            <p>User profile page.</p>
           </a>
         </div>
-      </main>
+        </Layout>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+
   )
 }
